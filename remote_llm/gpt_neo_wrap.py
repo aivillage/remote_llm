0c5e61e44161ac1f2d3e3cd51b38bcd166141ffc
Sven Cattell <scattell@gmail.com>
2023-02-23 12:07:09 -0500

    better defaults

--- remote_llm/gpt_neo_wrap.py
+++ remote_llm/gpt_neo_wrap.py
@@ -15,19 +15,24 @@ class GPTNeoWrap(AbstractLLM):
     model: AutoModelForCausalLM
     tokenizer: AutoTokenizer
     generator: TextGenerationPipeline
-    max_length: int 
+    max_new_tokens: int 
     num_sequences: int
+    max_tokens: int
 
-    def __init__(self, *, model: AutoModelForCausalLM, tokenizer: AutoTokenizer):
+    def __init__(self, *, model: AutoModelForCausalLM, tokenizer: AutoTokenizer, max_new_tokens = 50, max_tokens = 512, num_sequences = 1):
         self.model = model
         self.tokenizer = tokenizer
         self.generator = TextGenerationPipeline(model=model, tokenizer=tokenizer, device=0)
+        self.max_new_tokens = max_new_tokens
+        self.num_sequences = num_sequences
 
     def generate(self, prompts: List[str], stop: Optional[List[str]] = None) -> LLMResult:
         generations = []
         for prompt in prompts:
             generated = self.generator(
                 prompt,
+                max_new_tokens=self.max_new_tokens,
+                max_tokens=self.max_tokens,
             )
             generated = [Generation(text=gen['generated_text'][len(prompt):]) for gen in generated]
             generations.append(generated)
